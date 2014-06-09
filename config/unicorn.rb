ca14b4155fca8dd6bd41a01b4267ea86132fbbc6
Joe Pym <joe@joepym.com>
2014-06-09 18:48:08 -0400

    removed rails constant

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -2,8 +2,11 @@
 worker_processes Integer(ENV["WEB_CONCURRENCY"] || 3)
 timeout 15
 preload_app true
-pid Rails.root.join("tmp", "pids", "unicorn.pid")
-listen Rails.root.join("tmp", "sockets", "unicorn.sock")
+config_dir = File.symlink?(__FILE__) ? File.readlink(__FILE__) : __FILE__
+cwd = File.expand_path('..', config_dir)
+working_directory cwd
+pid cwd.join("tmp", "pids", "unicorn.pid")
+listen cwd.join("tmp", "sockets", "unicorn.sock")
 before_fork do |server, worker|
   Signal.trap 'TERM' do
     puts 'Unicorn master intercepting TERM and sending myself QUIT instead'
