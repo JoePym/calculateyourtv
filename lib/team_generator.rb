5b51953578634ba5984e9313af27e5e1d1a1c3c0
Joe Pym <joe@joepym.com>
2015-11-07 10:04:58 -0500

    Limited team name so page doesn't break

--- lib/team_generator.rb
+++ lib/team_generator.rb
@@ -10,9 +10,9 @@ class TeamGenerator
   def generate
     @pdf.image "#{Rails.root}/app/assets/images/#{@roster.logo_path}", :at => [0, @pdf.bounds.height], :height => 75
     @pdf.bounding_box([60, @pdf.bounds.height - 15], :width => 300, :height => 75) do
-      @pdf.text @team[:name].gsub("+", " "), :size => 27
+      @pdf.text @team[:name].gsub("+", " ").truncate(50), :size => 27
       @pdf.text "Coached By: #{@team[:coach]}"
-    end    
+    end
     team_details = [
       ["Team Gold", "#{@team[:gold]}k", "Apothecary", @team[:apo] == "true" ? "Yes" : "No"],
       ["Rerolls (#{@roster.reroll_cost.to_i}k):", @team[:rerolls], "Cheerleaders", @team[:cheerleaders]],
@@ -23,7 +23,7 @@ class TeamGenerator
     end
     num_col_width = 27
     widths = [num_col_width, 90, 130, num_col_width, num_col_width, num_col_width, num_col_width, 325, 40]
-    headers = [["", "Name", "Position", "Ma", 
+    headers = [["", "Name", "Position", "Ma",
              "St", "Ag", "Av", "Skills", "Cost"]]
     player_array = @team[:players].map do |index, player|
       @plus_ma = @plus_av = @plus_ag = @plus_st = 0
@@ -33,10 +33,10 @@ class TeamGenerator
       [ index.to_i + 1,
         player[:name],
         position.name,
-        @plus_ma > 0 ? green(position.ma + @plus_ma) : position.ma.to_s, 
-        @plus_st > 0 ? green(position.st + @plus_st) : position.st.to_s, 
-        @plus_ag > 0 ? green(position.ag + @plus_ag) : position.ag.to_s, 
-        @plus_av > 0 ? green(position.av + @plus_av) : position.av.to_s, 
+        @plus_ma > 0 ? green(position.ma + @plus_ma) : position.ma.to_s,
+        @plus_st > 0 ? green(position.st + @plus_st) : position.st.to_s,
+        @plus_ag > 0 ? green(position.ag + @plus_ag) : position.ag.to_s,
+        @plus_av > 0 ? green(position.av + @plus_av) : position.av.to_s,
         skills,
         player[:cost]
       ]
@@ -84,16 +84,16 @@ class TeamGenerator
         "<color rgb='#468847'>#{name}</color>"
       when "ma"
         @plus_ma += 1
-        "<color rgb='#F89406'>+#{name.titlecase.strip}</color>"     
+        "<color rgb='#F89406'>+#{name.titlecase.strip}</color>"
       when "av"
         @plus_av += 1
-        "<color rgb='#F89406'>+#{name.titlecase.strip}</color>"     
+        "<color rgb='#F89406'>+#{name.titlecase.strip}</color>"
       when "ag"
         @plus_ag += 1
-        "<color rgb='#BE06F8'>+#{name.titlecase.strip}</color>"     
-      when "st"     
-        @plus_st +=1 
-        "<color rgb='#953B39'>+#{name.titlecase.strip}</color>"  
+        "<color rgb='#BE06F8'>+#{name.titlecase.strip}</color>"
+      when "st"
+        @plus_st +=1
+        "<color rgb='#953B39'>+#{name.titlecase.strip}</color>"
       else
         name
       end
