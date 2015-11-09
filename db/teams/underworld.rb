4f1178c272da396a04141552fe1b3b0db18bc44e
Unibomber <alexanderweiss@gmx.de>
2015-11-09 17:09:52 -0500

    Fixed Skaven skills, reroll costs and numbering.
    Added Animosity, fixed Thrower skills.

--- db/teams/underworld.rb
+++ db/teams/underworld.rb
@@ -1,4 +1,4 @@
-underworld = Roster.create(:name => "Under-world", :reroll_cost => 60, :logo_path => "underworld_logo.png")
+underworld = Roster.create(:name => "Under-world", :reroll_cost => 70, :logo_path => "underworld_logo.png")
 
 puts "adding positionals to Underworld"
 troll = underworld.positions.create(:name => "Warpstone Troll",
@@ -8,16 +8,16 @@ troll = underworld.positions.create(:name => "Warpstone Troll",
   :maximum => 1,:cost => 110)
 blitzer = underworld.positions.create(:name => "Skaven Blitzer",
   :ma => 7, :st => 3, :ag => 3, :av => 8,
-  :default_skills => ["Block"],
+  :default_skills => ["Animosity", "Block"],
   :normal_skills => ["general", "strength", "mutation"], :double_skills => ["passing", "agility"],
   :cost => 90, :journeyman_position => false, :maximum => 2)
 thrower = underworld.positions.create(:name => "Skaven Thrower",
   :ma => 7, :st => 3, :ag => 3, :av => 7,
   :normal_skills => ["general", "passing", "mutation"], :double_skills => ["strength", "agility"],
-  :default_skills => ["Pass, Sure Hands"], :maximum => 2,:cost => 70)
+  :default_skills => ["Animosity", "Pass", "Sure Hands"], :maximum => 2,:cost => 70)
 lineman = underworld.positions.create(:name => "Skaven Lineman",
   :ma => 7, :st => 3, :ag => 3, :av => 7,
-  :default_skills => [],
+  :default_skills => ["Animosity"],
   :normal_skills => ["general", "mutation"], :double_skills => ["strength", "passing", "agility"],
   :maximum => 2,:cost => 50)
 goblin = underworld.positions.create(:name => "Underworld Goblin",
@@ -55,8 +55,8 @@ end
   team.players.create(:name => names[3+i], :position => thrower, :number => 4 + i)
 end
 2.times do |i|
-  team.players.create(:name => names[5+i], :position => lineman, :number => 8 + i)
+  team.players.create(:name => names[5+i], :position => lineman, :number => 6 + i)
 end
 6.times do |i|
-  team.players.create(:name => names[7+i], :position => goblin, :number => 10+ i)
+  team.players.create(:name => names[7+i], :position => goblin, :number => 8+ i)
 end
